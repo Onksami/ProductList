a55352e322e392ecb06a283e576ce8b2f9725dd8
irfan önk <onk.irfan@gmail.com>
2023-11-16 22:30:32 +0300

    add sidebar

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import ProductList from "./Components/ProductList";
 import Pagination from "./Components/Pagination";
 import { useContext } from "react";
 import { ProductContext } from "./Context/ProductContext";
+import Sidebar from "./Components/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <ProductList />
+      <div className="app-container">
+        <div className="sidebar">
+          <Sidebar />
+        </div>
+        <div>
+          <ProductList />
 
-      <Pagination />
+          <Pagination />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <div>box 1</div>
+      <div>box 2</div>
+      <div>box 3</div>
+      <div>box 4</div>
+    </div>
+  );
+};
+
+export default Sidebar;
