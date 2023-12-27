862e3206c423ea4cad0839684dfc7136b3860f9c
irfan önk <onk.irfan@gmail.com>
2023-12-27 16:31:04 +0300

    add to card

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,65 +1,55 @@
 import React, { useState, useEffect, useContext } from "react";
 import { ProductContext } from "../Context/ProductContext";
 
-
 const ProductCard = (props) => {
+  //context
+  const productContext = useContext(ProductContext);
+  // console.log("ProuctCards  productContext:", productContext);
 
-//context
-const productContext = useContext(ProductContext);
-// console.log("ProuctCards  productContext:", productContext);
+  //function
 
-//function
+  // Converting number to date
 
+  let storedTimestamp = props.item.added; // Assuming 'timestamp' is the key where the number is stored
+  let date = new Date(parseInt(storedTimestamp, 10));
 
-// Converting number to date
+  // Functions
 
-let storedTimestamp = props.item.added // Assuming 'timestamp' is the key where the number is stored
-let date = new Date(parseInt(storedTimestamp, 10));
+  const shoppingCardAdd = () => {
+    //1. get previous shopping card from context
+    const shoppingCard = [...productContext.shoppingCard];
 
+    // 2. check if item is already added
+    const addedItem = shoppingCard.find(
+      (item) => item.name === props.item.name
+    );
 
-// Functions
+    if (!addedItem) {
+      // 3. add quantity key inside item object
+      props.item.quantity = 1;
+      shoppingCard.push(props.item);
+      productContext.setShoppingCard(shoppingCard);
+    } else {
+      addedItem.quantity += 1;
+      productContext.setShoppingCard(shoppingCard);
+    }
 
-const shoppingCardAdd = () => {
-  //get previous shopping card from context
-  const shoppingCard = [...productContext.shoppingCard];
-
-  console.log("shopping card before push", shoppingCard);
-
-  //push new item to previous shopping card
-  shoppingCard.push(props.item)
-  console.log("shopping card after push", shoppingCard);
-  
-  // set new shopping card to context state
-
-  productContext.setShoppingCard(shoppingCard);
-  console.log("Add button clicked");
-}
-
+    // @todo calculate total prize and set to context
+  };
 
   return (
     <div className="product-card">
-      
-    <div className="pcImage">
+      <div className="pcImage">
+        <img alt="asd" src="https://picsum.photos/id/83/200/300" />
+      </div>
 
-      <img alt="asd" src="https://picsum.photos/id/83/200/300"/>
-    
-    </div>
+      <div className="pcPrice">₺ {props.item.price}</div>
 
-    <div className="pcPrice">
-     ₺ {props.item.price}
-    </div>
+      <div className="pcName">{props.item.name}</div>
 
-    <div className="pcName">
-     {props.item.name}
-    </div>
-
-    <div className="pcButton">
-    <button onClick={shoppingCardAdd}>Add to Card</button>
-    </div>
-    
-    
-
- 
+      <div className="pcButton">
+        <button onClick={shoppingCardAdd}>Add to Card</button>
+      </div>
     </div>
   );
 };
--- src/Components/ShoppingCartList.jsx
+++ src/Components/ShoppingCartList.jsx
@@ -1,52 +1,46 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext } from "react";
 import { ProductContext } from "../Context/ProductContext";
 
-
 function ShoppingCartList() {
-
-    const productContext = useContext(ProductContext);
-    // console.log("ShoppingCartList" , productContext);
+  const productContext = useContext(ProductContext);
+  // console.log("ShoppingCartList" , productContext);
 
   const [quantity, setQuantity] = useState(1);
   // const productName = productContext.shoppingCard.name;
   // const price = productContext.shoppingCard.price; // You can set the price dynamically or statically
   // const totalPrice = price * quantity
 
-
-
   const increaseQuantity = () => {
-    setQuantity(prevQuantity => prevQuantity + 1);
+    //1. find selected shopping item inside context shoppingCard
+    //2. increase quantity of selected shopping item
+    //3. update context shoppingCard (setShoppingCard)
   };
 
   const decreaseQuantity = () => {
-    if (quantity > 0) {
-      setQuantity(prevQuantity => prevQuantity - 1);
-    }
+    //1. find selected shopping item inside context shoppingCard
+    //2. decrease quantity of selected shopping item
+    //3. update context shoppingCard (setShoppingCard)
+    // @notice if quantity is 1 then remove from context shoppingCard
+    // how to remove an object from array
   };
 
   console.log("shopping card coming? ", productContext.shoppingCard);
 
   return (
     <>
-    {productContext.shoppingCard.map((product, index) =>{
-      return (
-        <div>{product.name} <br />
-        Price: ${product.price}
+      {productContext.shoppingCard.map((product, index) => {
+        return (
+          <div>
+            {product.name} <br />
+            Price: ${product.price}
+            <button onClick={decreaseQuantity}>-</button>
+            {product?.quantity}
+            <button onClick={increaseQuantity}>+</button>
+          </div>
+        );
+      })}
 
-
-        
-        <button onClick={decreaseQuantity}>-</button>
-        1
-        <button onClick={increaseQuantity}>+</button>
-    </div> 
-      )
-    } )}
-
-      <div>
-        Total: 
-      </div>
-
-
+      <div>Total:</div>
     </>
   );
 }
